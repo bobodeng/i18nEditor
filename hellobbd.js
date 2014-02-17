d97ea57991057c45428b6fa4f464d21abda0bbe7
bbd <bobodeng2008@gmail.com>
2014-02-17 23:20:45 +0800

    #bbd bug fix
    正则匹配 贪婪

--- hellobbd.js
+++ hellobbd.js
@@ -3,34 +3,73 @@
     console.log(obj);
  }
 
+ // 载入样式
 var link = document.createElement("link");
 link.rel = "stylesheet";
 link.type = "text/css";
-link.href = "http://www.baihuzi.com/baihuzi/i18n.css";
+link.href = "http://www.baihuzi.com/baihuzi/i18n.css?t=7";
 document.getElementsByTagName("head")[0].appendChild(link);
 
  seajs.config({
     alias: {
         '$': 'gallery/jquery/1.7.2/jquery',
         'popup': 'arale/popup/1.1.6/popup',
-        'overlay' : 'arale/overlay/1.1.4/overlay',
         'sticky' : 'arale/sticky/1.3.1/sticky',
-        'tabs' : 'arale/switchable/1.0.2/tabs'
-
-    // 'position': 'http://assets.dev.alipay.net/arale/position/1.0.0/position'
-
+        'tabs' : 'arale/switchable/1.0.2/tabs',
+        'uri' : 'gallery/jsuri/1.2.2/jsuri'
     }
     // base:"https://a.alipayobjects.com/"
   });
 
-seajs.use(['$','popup','overlay','sticky','tabs'], function($,Popup,Overlay,Sticky,Tabs) {
+seajs.use(['$','popup','sticky','tabs','uri'], function($,Popup,Sticky,Tabs,Uri) {
     log("hello i18nEdit~");
     // 数据源操作
-// SecurityForm.answer[not.blank]=Please enter your answer.
-// SecureQuestionForm.email[not.blank]=Please enter your email.
-// SecureQuestionForm.email[not.null]=Please enter your email.
-// SecureQuestionForm.email[regexp]=Email not recognized, please enter again.
-// SecureQuestionForm.email[length]=Email not recognized, please enter again.
+    var i18nEditor = {
+        version : "1.0.0",
+        author : "#bbd {玉郎}",
+
+        // 根据用户选择，判断是否打开编辑模式
+        init : function(){
+            // ##转译替换特殊字符 jsuri目前有bug 当url里出现@符号，会导致获取不到get参数，这是临时方案
+            var stripString = function(s) {
+            var reg = new RegExp("[@]");
+            var rs = "";
+            for (var i = 0; i < s.length; i++) {
+                 rs = rs + s.substr(i, 1).replace(reg,'{{email}}');
+              }
+            return rs;
+            }
+            var resolveString = function(s) {
+                var _s = unescape(s)+"";
+                var regS = new RegExp("{{email}}","gi");
+                _s = _s.replace(regS,"@")
+                return _s;
+            }
+
+            var uri = new Uri(stripString(window.location.href));
+            var _needResolve = true;
+
+            if(data.loadEditMode() == "false"){
+                 if(uri.getQueryParamValue('edit')){
+                    uri.deleteQueryParam('edit');
+                    window.location.href = resolveString(uri.toString());
+                }
+                _needResolve = false;
+            }else{
+                if(uri.getQueryParamValue('edit')){
+                    // uri.deleteQueryParam('edit');
+                }else{
+                    uri.addQueryParam('edit','i18n');
+                    window.location.href = resolveString(uri.toString());
+                }
+            }
+
+            return _needResolve;
+            
+        }
+    }
+
+    //主体模块开始
     var data = {
         // 原始数据，有后端支持后初始化这里
         obj : {
@@ -50,10 +89,60 @@ seajs.use(['$','popup','overlay','sticky','tabs'], function($,Popup,Overlay,Stic
             _slef["history"][lang][key] = value;
             _slef.setValue(key,value);
         },
+        saveEditMode : function(value){
+            var _slef = this;
+            var _storage =  window.localStorage;
+            _storage["eidtMode"] = value;
+        },
+        loadEditMode : function(){
+            var _slef = this;
+            var _storage =  window.localStorage;
+            return _storage["eidtMode"];
+        },
+        getHistory : function(lang){
+            var _slef = this;
+            if(lang){
+                return _slef["history"][lang];
+            }else{
+                return _slef["history"];
+            }
+        },
+        setHistory : function(data){
+            var _slef = this;
+            _slef["history"] = data;
+        },
         getCurrentHistory : function(){
             var _slef = this;
             var _lang = _slef.getLang();
             return _slef["history"][_lang];
+        },
+        saveI18nLocal : function(){
+            var _slef = this;
+            var _storage =  window.localStorage;
+            _storage["ihome"] = JSON.stringify(_slef.getHistory());
+            // JSON.stringify 把一个对象转换成json字符串， 
+            // JSON.parse 把一个json字符串解析成对象。
+        },
+        delLocal : function(){
+            var _slef = this;
+            var _storage =  window.localStorage;
+            _storage.clear();
+        },
+        // 读取本地缓存，并修改当前数据源
+        loadI18nLocal : function(){
+            var _slef = this;
+            var _storage = window.localStorage;
+            var _history = _storage["ihome"];
+            // 存储到当前操作记录中
+            if(_history && _history != ""){
+                var _historyObj = JSON.parse(_history);
+                _slef.setHistory(_historyObj);
+
+                $.each(_slef.getCurrentHistory(),function(n,v){
+                    // 给当前的数据源赋值
+                    _slef.setValue(n,v);
+                })               
+            }
         },
 
         getValue : function(key){
@@ -74,30 +163,102 @@ seajs.use(['$','popup','overlay','sticky','tabs'], function($,Popup,Overlay,Stic
             _slef["lang"] = lang;
         },
         init : function(){
+            var _slef = this;
+            // 获取当前语言，并存储 TODO 目前从dom内获取，未来要修改成读参数
+            var _lang =$("#trigger-language").text();
+            if(_lang.indexOf("中文(简体)")!= -1){
+                _lang = "zh_CN";
+            }else if(_lang.indexOf("English")!= -1){
+                _lang = "en_US";
+            }else if(_lang.indexOf("Pусский")!= -1){
+                _lang = "u_RU";
+            }else if(_lang.indexOf("Português")!= -1){
+                _lang = "pt_PT";
+            }else if(_lang.indexOf("España")!= -1){
+                _lang = "p_SP";
+            }else{
+                _lang = "en_US";
+            }
+           
+            data.setLang(_lang);
+            log("当前语言是"+_lang);   
+
+            // 读取本地缓存，并修改当前数据源
+            _slef.loadI18nLocal();
+
             // TODO : 待接入后端文件支持
         }
 
     };
 
+
+
+
     // i18nEdit 页面解析
     var resolve = {
 
+        // 根据正则转译当前页面的key，返回需要的代码片段 插入body
         execRE : function (re, rp, content) {
+            // TODO 这坨逻辑改的很渣，需要优化
+           
+            // 替换{{{{value____key}}}} --><span key>value</span>
             var _slef = this;
-            oReg = new RegExp(re, "ig");
-            r = content.replace(oReg, function(words){
-                // 截取key value ,存储data
-                var _point = words.slice(4,words.length-4);
-                var _arr = _point.split("====");
-                var _key = _arr[0];
-                var _value = _arr[1];
+            var _history = data.getCurrentHistory() || {};
+            var oReg = new RegExp(re, "ig");
+            
+            // 收集页面中储存在JS或者Dom中的key value
+            var _hideArr = [];
 
-                data.setValue(_key,_value);
-                var _result = words[0]+"<span data-key='"+_key+"' class='j-i18n'>"+data.getValue(_key)+"</span>"+ words[words.length-1];
+            // 传入value____key,返回dom结构, TODO 不得已加入了对比修改记录逻辑，容后修改
+            var wordsToHtml = function(point){
+                var _point = point;
+                var _arr = _point.split("____");
+                var _value = _arr[0];
+                var _key = _arr[1];
+
+                // 对比当前是否有修改记录，如果有，就以修改后的值为准，如果没有，存储页面中取到的值
+                if(_history[_key] && _history[_key] != _value){
+                    _value = _history[_key];
+                }else{
+                    data.setValue(_key,_value);
+                }
+                var _point ="<span data-key='"+_key+"' class='j-i18n'>"+data.getValue(_key)+"</span>";
+
+                return _point;
+            }
+            
+            // 防止误改，先过滤掉标签中的key|value ，替换掉""中的{}，并暂存，
+            content = content.replace(/[\"|\']\s*\{{3}.*?\}{3}\s*/g,function(words){
+                _resultWords = words.replace(/[\{|\}]/g,"");
+                _hideArr.push(words);
+                return _resultWords;
+            })
+
+
+            _result = content.replace(oReg, function(words){
+                // 截取key value ,存储data
+                var _point = wordsToHtml(words.slice(4,words.length-4))
+                // 拼回多截取的部分
+                var _result = words[0]+_point+ words[words.length-1];
                 return _result;
             });
-            return r; 
+
+            // 在页面底部增加一个缓存当前不可见文案的区域
+            var _html = '<div class="temp-box fn-clear"><ul><p>本页面中的隐藏文案：</p>';
+            // 此时的_hideArr中存储的是首字母带"的value____key
+            $.each(_hideArr,function(n,v){
+                // 截取key value ,存储data
+                log(v)
+                // 替换掉多余的{ }
+                v = v.replace(/[\"|\'|\{|\}]/g,"");
+                var _point = wordsToHtml(v)
+                _html = _html+'<li>'+_point+'</li>';
+            })
+            _result = _result + _html +'</ul></div>';
+
+            return _result; 
         },
+        // 编辑主体函数
         i18nEditBox : function(){
              var jPop = new Popup({
                 trigger: '.j-i18n',
@@ -105,12 +266,20 @@ seajs.use(['$','popup','overlay','sticky','tabs'], function($,Popup,Overlay,Stic
                 beforeShow: function() {
                     // this.element.html(this.activeTrigger.data('key'));
                     // 获取当前新内容
+                    var _textarea  = $("textarea",this.element)
                     $("p",this.element).text(this.activeTrigger.data('key')+" = ");
-                    $("textarea",this.element).val(this.activeTrigger.html());
+                    _textarea.val(this.activeTrigger.html());
+                    _textarea.bind('keyup', function(event) {
+                        // 快捷键：回车出发关闭浮层事件
+                        if (event.keyCode == 13) {
+                            jPop.hide();
+                        }
+                            
+                    })
                 },
                 // afterHide目前有bug，会show hide时各回调一次 
                 afterHide : function(){
-                    if($("textarea",this.element).val() =="" || $("textarea",this.element).val() == this.activeTrigger.html()){
+                    if($("textarea",this.element).val().trim() =="" || $("textarea",this.element).val().trim() == this.activeTrigger.html().trim()){
                         return ;
                     }else{
                         // 改变文案并且记录history，修改data
@@ -127,10 +296,9 @@ seajs.use(['$','popup','overlay','sticky','tabs'], function($,Popup,Overlay,Stic
                     selfXY: [10, "100%+10"]
                 }
             });
-            $(".j-i18n").click(function(e){
-                e.preventDefault();
-            })
+
         },
+        // 获取当前鼠标选中内容
         _getSelectText : function (){
             var txt = null;
             if (window.getSelection){  // mozilla FF
@@ -147,18 +315,78 @@ seajs.use(['$','popup','overlay','sticky','tabs'], function($,Popup,Overlay,Stic
         init : function(){
             var _slef = this;
             var _html = $("body").html();
-            // key to value
-            $("body").html( _slef.execRE("[^\"|^\']\{{3}.*\}{3}[^\"|^\']","*",_html))
-            // popup
+            // 根据正则转译当前页面的key to value
+            _html
+            $("body").html( _slef.execRE("[^\"|^\']\s*\{{3}.*?\}{3}[^\"|^\']","*",_html))
+            // popup事件，绑定修改事件
             _slef.i18nEditBox();
         }
     }
+
+
+
 
     // 修改记录板
     var blackBoard = {
         init : function(){
             var _slef = this;
+
+            // 渲染语言tab
+            _slef._initTab();
+            
+            // i18nBoard 开关事件
+            $("#i18nBoard .j-board-trigger").click(function(e){
+                _slef.triggerBoard();
+            })
+
+            $(window).bind('keyup', function(event) {
+                // if (event.keyCode == 27 && event.ctrlKey) {
+                // 快捷键：ESC
+                if (event.keyCode == 27) {
+                    _slef.triggerBoard();
+                }
+                    
+            })
+            // 删除修改记录
+            $("#i18nBoard .j-del-local").click(function(e){
+                if(confirm("将删除目前记录的所有操作，并刷新页面，请确认。")){
+                    data.delLocal();
+                    window.location.reload();
+                }
+            })
+            
+            // 储存editmode 到 local ,重新渲染页面
+            $("#eidtModeTrigger").click(function(e){
+                data.saveEditMode($(this)[0].checked+"");
+                window.location.reload();
+
+            })
+            // TODO :调研一下，是否需要储存是否禁止点击事件
+            $("#preventDefaultTrigger").click(function(e){
+
+            })
+            // 根据禁用链接状态，判断是否禁掉多语言的链接
+            $(".j-i18n").click(function(e){
+                if($("#preventDefaultTrigger")[0].checked){
+                    e.preventDefault();
+                    return false;
+                }
+            })
+
+            // 历史操作记录初始化
+            _slef.refreshBoard("init");
+
+        },
+        _initTab : function(){
+            var _slef =this;
             var _str = '<div id="i18nBoard" class="">'+
+                '<div class="edit-info">'+
+                '    <span>version :<span class="board-key">'+i18nEditor["version"]+'</span> 有问题联系:<span class="board-key">'+i18nEditor["author"]+'</span></span>  |  '+
+                '    <span>你现在编辑的语言是：<span class="board-key">'+ data.getLang() +'</span></span>  |  '+
+                '    <span> 开关操作记录的快捷键：<span class="board-key">ESC</span>   | '+
+                '    <span title="关闭后，将不在显示编辑框"><input type="checkbox" id="eidtModeTrigger" name="eidtMode" checked="checked"> 编辑模式</span>'+
+                '    <span title="遇到不能点击的链接，请去掉勾选"><input type="checkbox" id="preventDefaultTrigger" name="preventDefault" checked="checked"> 禁止编辑时跳转</span>'+
+                '</div>'+
                 '<div class="content">'+
                 '<ul class="ui-switchable-nav">'+
                 '    <li data-lang="zh_CN">中文(简体)</li>'+
@@ -177,50 +405,29 @@ seajs.use(['$','popup','overlay','sticky','tabs'], function($,Popup,Overlay,Stic
                 '    </div>'+
                 '</div>'+
                 '</div>'+
-                '<div class="j-board-trigger board-trigger">trigger</div>'
+                '<ul class="botton-list">'+
+                '    <li class="j-del-local" title="删除修改记录">删除</li>'+
+                // '    <li class="j-save-local" title="保存本次修改">保存</li>'+
+                '    <li class="j-board-trigger" title="快捷键：ESC">展开</li>'+
+                '</ul>'+
             '</div>';
 
-            // <ul class="ui-switchable-nav">
-            //     <li data-lang="zh_CN">中文(简体)</li>
-            //     <li data-lang="en_US">English</li>
-            //     <li data-lang="ru_RU">Pусский</li>
-            //     <li data-lang="pt_PT">Português</li>
-            //     <li data-lang="sp_SP">España</li>
-            // </ul>
-            // <div class="ui-switchable-content">
-            //     <div class="fn-hide">
-            //         内容 A
-            //     </div>
-            //     <div class="fn-hide">内容 B</div>
-            //     <div class="fn-hide">内容 C</div>
-            //     <div class="fn-hide">内容 D</div>
-            //     <div class="fn-hide">内容 E</div>
-            // </div>
             $(_str).appendTo("body");
             // Sticky.fix("#i18nBoard"); 
 
-            tabs = new Tabs({
+            var tabs = new Tabs({
                 element: '#i18nBoard',
                 triggers: '.ui-switchable-nav li',
                 panels: '.ui-switchable-content div',
                 triggerType : 'click',
-                activeIndex: 2,
+                activeIndex: 0,
                 effect: 'fade',
                 // effect: 'scrollx',
                 interval: 3000
             });
-            $(".j-board-trigger").click(function(e){
-                _slef.triggerBoard();
-            })
 
-            $(window).bind('keyup', function(event) {
-                log(event.keyCode);
-                // if (event.keyCode == 27 && event.ctrlKey) {
-                if (event.keyCode == 27) {
-                    _slef.triggerBoard();
-                }
-                    
-             })
+            // 读取编辑模式上次的状态
+            if(data.loadEditMode() == "false") $("#eidtModeTrigger")[0].checked = false;
 
         },
         triggerBoard : function(){
@@ -228,34 +435,45 @@ seajs.use(['$','popup','overlay','sticky','tabs'], function($,Popup,Overlay,Stic
             var _board = $("#i18nBoard");
             if(_board.hasClass("show")){
                 $("#i18nBoard").animate({height:"30px"}).removeClass("show")
+                $(".j-board-trigger").text("展开");
             }else{
                 $("#i18nBoard").animate({height:"400px"}).addClass("show")   
+                $(".j-board-trigger").text("收起");
             }
         },
-        saveLocal : function(){
-            var _slef = this;
-
-        },
-        resolveLocal : function(){
-            var _slef = this;
-            
-
-        },
-        delLocal : function(){
-            var _slef = this;
-
-
-        },
-        refreshBoard  : function(){
+        refreshBoard  : function(init){
             var _slef = this;
             var _lang = data.getLang();
-            var _lsitObj = data.getCurrentHistory();
-            var _board = $("#i18nBoard #"+_lang+"-box");
-            var _str = "";
-            $.each(_lsitObj,function(n,v){
-                _str = _str + "<p>"+n+"="+v+"</p>";
-            })
-            _board.html(_str);
+            
+            if(init){
+                // 初始化状态下，渲染所有语言类的操作记录
+                var  _langsArr = [];
+                $.each($("#i18nBoard .ui-switchable-nav li"),function(n,v){
+                    _langsArr.push($(v).data("lang"));
+                })
+                $.each(_langsArr,function(n,v){
+                    // v - zh_CN ……
+                    var _str = "";
+                    var _lsitObj = data.getHistory(v);
+                    if(_lsitObj){
+                        $.each(_lsitObj,function(nn,vv){
+                            _str = _str + "<p><span class='board-key'>"+nn+"</span>=<span class='board-value'>"+vv+"</span></p>";
+                        })
+
+                        $("#i18nBoard #"+v+"-box").html(_str);
+                    }
+                })
+
+            }else{
+                // 非初始化状态，重新渲染当前语言的操作记录
+                var _lsitObj = data.getCurrentHistory();
+                var _str = "";
+                $.each(_lsitObj,function(n,v){
+                    _str = _str + "<p><span class='board-key'>"+n+"</span>=<span class='board-value'>"+v+"</span></p>";
+                })
+                $("#i18nBoard #"+_lang+"-box").html(_str);
+                data.saveI18nLocal();
+            }
 
 
         },
@@ -265,15 +483,21 @@ seajs.use(['$','popup','overlay','sticky','tabs'], function($,Popup,Overlay,Stic
         },
         delTask : function(){
             var _slef = this;
-
         }
-
-
     }
 
-    // init
-    resolve.init();
+// init
+    // 数据源初始化
+    data.init()
+    var _needResolve = i18nEditor.init();
+
+    // 编辑主体事件初始化
+    if(_needResolve){
+        resolve.init();
+    }
+    // 操作板初始化
     blackBoard.init(); 
+
 
 });
 
